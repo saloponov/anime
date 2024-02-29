b361e91de995ab4072faebee511a34c5c013d507
Saloponov <saloponov.ivan@gmail.com>
2024-02-29 11:20:27 +0300

    Поиск по базе данных и отображение в списке рядом с окнном поиска

--- js/modal.js
+++ js/modal.js
@@ -3,7 +3,18 @@ const modal = () => {
     const modalSearchBtn = document.querySelector('.search-switch')
     const modalSearchClose = document.querySelector('.search-close-switch')
     const modalSearchInput = document.querySelector('#search-input')
+    const wrapper = document.querySelector('.search-model-result')
 
+    console.log(wrapper);
+    let database;
+    
+    fetch('https://anime-database-8ca84-default-rtdb.europe-west1.firebasedatabase.app/anime.json').then((response) => {
+        return response.json()
+    }).then((data) => {
+        database = data;
+    });
+
+    console.log(database);
 
     const modalSearchAnime = () => {
         console.log(modalSearch.classList)
@@ -23,14 +34,29 @@ const modal = () => {
         });
     }
 
+    const searchAnimeInDb = (searchStr) => {
+        database.filter((record) => {
+            return record.title.toLowerCase().includes(searchStr.toLowerCase()) ||
+            record.description.toLowerCase().includes(searchStr.toLowerCase());
+        }).forEach(item => {
+            console.log(item);
+            wrapper.insertAdjacentHTML('afterbegin', `
+            <a class"p-2" href="/anime-details.html" target="_blank">${item.title}</a>
+            `);
+        });    
+    }
+
     modalSearchClose.addEventListener('click', () => {
         console.log('modalSearchClose.addEventListener')
 
         modalSearchAnime();
     });
+    
 
     modalSearchInput.addEventListener('input', () => {
         console.log(modalSearchInput.value);
+
+        searchAnimeInDb(modalSearchInput.value);
     })
 
     modalSearchOpenFn();
